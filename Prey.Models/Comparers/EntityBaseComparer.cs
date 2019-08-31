933b155b678e2b3c23a5d91f3b446062ae58812c
Leon <Leon.ID@QQ.COM>
2019-08-31 20:18:24 +0800

    增加实体基类比较器；

---
+++ Prey.Models/Comparers/EntityBaseComparer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Prey.Models.Comparers
+{
+    /// <summary>
+    /// 实体基类比较器
+    /// </summary>
+    /// <typeparam name="TEntityBase">实体基类类型</typeparam>
+    public class EntityBaseComparer<TEntityBase> : IEqualityComparer<TEntityBase>
+        where TEntityBase : EntityBase
+    {
+        /// <summary>
+        /// 相等
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public virtual bool Equals(TEntityBase x, TEntityBase y)
+            => string.Equals(x?.ID, y?.ID);
+
+        /// <summary>
+        /// 获取哈希
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public virtual int GetHashCode(TEntityBase obj)
+            => obj.GetHashCode();
+    }
+}
