94568a9a0d3cbeb3a141a0c27466c1a601709bb9
Leon <Leon.ID@QQ.COM>
2019-09-01 12:47:07 +0800

    增加一个认证扩展项目，用于实现独立的用户认证功能；

---
+++ Prey.Authentication/Extension/LeonAuthenticationExtension.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Prey.Authentication.Middleware;
+
+namespace Prey.Authentication.Extension
+{
+    /// <summary>
+    /// Leon 认证中间件扩展
+    /// </summary>
+    public static class LeonAuthenticationExtension
+    {
+        /// <summary>
+        /// 应用 Leon 认证到 HTTP 请求管道
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <returns></returns>
+        public static IApplicationBuilder UseLeonAuthentication(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<LeonAuthenticationMiddleware>();
+        }
+    }
+}
---
+++ Prey.Authentication/Middleware/LeonAuthenticationMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Prey.Authentication.Middleware
+{
+    /// <summary>
+    /// Leon 认证中间件
+    /// </summary>
+    public class LeonAuthenticationMiddleware
+    {
+        /// <summary>
+        /// 下一 HTTP 请求委托
+        /// </summary>
+        private readonly RequestDelegate next;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LeonAuthenticationMiddleware"/> class.
+        /// </summary>
+        /// <param name="next"></param>
+        public LeonAuthenticationMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        /// <summary>
+        /// 调用
+        /// </summary>
+        /// <param name="httpContext"></param>
+        /// <returns></returns>
+        public Task Invoke(HttpContext httpContext)
+        {
+
+            return this.next(httpContext);
+        }
+    }
+}
